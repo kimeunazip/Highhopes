8f650c2187812013e8e242eb62a27d7d7172d0f8
kimeuna <107541225+kimeunazip@users.noreply.github.com>
2023-05-29 23:27:28 +0900

    Toy

---
+++ DentbillProject/src/main/java/com/dentalLab/ERPproject/ErPprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dentalLab.ERPproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErPprojectApplication.class, args);
+	}
+
+}
---
+++ DentbillProject/src/test/java/com/dentalLab/ERPproject/ErPprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalLab.ERPproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErPprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
